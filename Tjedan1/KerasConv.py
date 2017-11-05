fca001751ec2a4ab501ebf129c85030f5dc97ac6
Domjan Baric <domjanbaric@live.com>
2017-11-05 19:54:29 +0100

    Dodani zadatci za drugi tjedan

---
+++ Tjedan1/KerasConv.py
@@ -0,0 +1,183 @@
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+# load (downloaded if needed) the MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# plot 4 images as gray scale
+plt.subplot(221)
+plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
+plt.subplot(222)
+plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
+plt.subplot(223)
+plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
+plt.subplot(224)
+plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
+# show the plot
+plt.show()
+
+
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import np_utils
+
+seed = 7
+numpy.random.seed(seed)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+
+
+#################################################################
+#Conv
+#######################################
+
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+seed = 7
+numpy.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# reshape to be [samples][pixels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+
+# build the model
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("CNN Error: %.2f%%" % (100 - scores[1] * 100))
+
+########################################
+#CIFAR
+##########################################
+
+from keras.datasets import cifar10 # subroutines for fetching the CIFAR-10 dataset
+from keras.models import Model # basic class for specifying and training a neural network
+from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+import numpy as np
+
+batch_size = 32 # in each iteration, we consider 32 training examples at once
+num_epochs = 200 # we iterate 200 times over the entire training set
+kernel_size = 3 # we will use 3x3 kernels throughout
+pool_size = 2 # we will use 2x2 pooling throughout
+conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
+conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
+drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
+drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
+hidden_size = 512 # the FC layer will have 512 neurons
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data() # fetch CIFAR-10 data
+
+num_train, height, width, depth = X_train.shape # there are 50000 training examples in CIFAR-10
+num_test = X_test.shape[0] # there are 10000 test examples in CIFAR-10
+num_classes = np.unique(y_train).shape[0] # there are 10 image classes
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= np.max(X_train) # Normalise data to [0, 1] range
+X_test /= np.max(X_test) # Normalise data to [0, 1] range
+
+Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
+Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+
+inp = Input(shape=(height, width, depth)) # depth goes last in TensorFlow back-end (first in Theano)
+# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(conv_1)
+pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+drop_1 = Dropout(drop_prob_1)(pool_1)
+# Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
+conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
+conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(conv_3)
+pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
+drop_2 = Dropout(drop_prob_1)(pool_2)
+# Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
+flat = Flatten()(drop_2)
+hidden = Dense(hidden_size, activation='relu')(flat)
+drop_3 = Dropout(drop_prob_2)(hidden)
+out = Dense(num_classes, activation='softmax')(drop_3)
+
+model = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
+
+model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+              optimizer='adam', # using the Adam optimiser
+              metrics=['accuracy']) # reporting the accuracy
+
+model.fit(X_train, Y_train,                # Train the model using the training set...
+          batch_size=batch_size, epochs=num_epochs,
+          verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
+model.evaluate(X_test, Y_test, verbose=1)  # Evaluate the trained model on the test set!
+
+
+
+
+
+
+
+
+
+
